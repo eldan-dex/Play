d918dbbe5a0033d8598dd6add5e3a3fc00bf28e6
Dex@Twi <eldan-dex@users.noreply.github.com>
2019-12-12 22:26:19 +0100

    trying to merge #1

--- UltraStar Play/Assets/Common/Model/Stats/TopEntry.cs
+++ UltraStar Play/Assets/Common/Model/Stats/TopEntry.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -32,3 +33,39 @@ public class CompareByTopEntryScore : IComparer<TopEntry>
         return x.SongStatistic.Score.CompareTo(y.SongStatistic.Score);
     }
 }
+=======
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Holds data about one of the "top" songs
+[Serializable]
+public class TopEntry
+{
+    public string songName { get; private set; }
+    public string songArtist { get; private set; }
+    public SongStatistic songStatistic { get; private set; }
+
+    public TopEntry(string songName, string songArtist, SongStatistic songStatistic)
+    {
+        this.songName = songName;
+        this.songArtist = songArtist;
+        this.songStatistic = songStatistic;
+    }
+}
+
+//Comparer for score sorting
+public class CompareByTopEntryScore : IComparer<TopEntry>
+{
+    public int Compare(TopEntry x, TopEntry y)
+    {
+        if (x.songStatistic == null || y.songStatistic == null)
+        {
+            return 0;
+        }
+
+        return x.songStatistic.Score.CompareTo(y.songStatistic.Score);
+    }
+}
+>>>>>>> parent of 5e3c645... Improved highscore display in the song select
--- UltraStar Play/Assets/Scenes/SongSelect/SongSelectSceneController.cs
+++ UltraStar Play/Assets/Scenes/SongSelect/SongSelectSceneController.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using System;
@@ -246,3 +247,258 @@ public class SongSelectSceneController : MonoBehaviour, IOnHotSwapFinishedListen
         return searchTextInputField.isActiveAndEnabled;
     }
 }
+=======
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using System.Linq;
+using UniRx;
+
+public class SongSelectSceneController : MonoBehaviour, IOnHotSwapFinishedListener
+{
+    public static SongSelectSceneController Instance
+    {
+        get
+        {
+            return FindObjectOfType<SongSelectSceneController>();
+        }
+    }
+
+    public ArtistText artistText;
+    public Text songTitleText;
+    public Text songCountText;
+    public HighscoreLocalPlayerText highscoreLocalPlayer;
+    public HighscoreLocalScoreText highscoreLocalScore;
+    public HighscoreWebPlayerText highscoreWebPlayer;
+    public HighscoreWebScoreText highscoreWebScore;
+    public GameObject videoIndicator;
+    public GameObject duetIndicator;
+
+    public SongSelectPlayerProfileListController playerProfileListController;
+
+    private SearchInputField searchTextInputField;
+
+    private SongRouletteController songRouletteController;
+
+    private SongSelectSceneData sceneData;
+    private List<SongMeta> songMetas;
+    private Statistics statsManager;
+
+    private SongMeta selectedSongBeforeSearch;
+
+    public GameObject noSongsFoundMessage;
+
+    private SongMeta SelectedSong
+    {
+        get
+        {
+            return songRouletteController.Selection.Value.SongMeta;
+        }
+    }
+
+    void Start()
+    {
+        sceneData = SceneNavigator.Instance.GetSceneData(CreateDefaultSceneData());
+
+        searchTextInputField = GameObjectUtils.FindObjectOfType<SearchInputField>(true);
+
+        highscoreLocalPlayer = GameObjectUtils.FindObjectOfType<HighscoreLocalPlayerText>(true);
+        highscoreLocalScore = GameObjectUtils.FindObjectOfType<HighscoreLocalScoreText>(true);
+        highscoreWebPlayer = GameObjectUtils.FindObjectOfType<HighscoreWebPlayerText>(true);
+        highscoreWebScore = GameObjectUtils.FindObjectOfType<HighscoreWebScoreText>(true);
+
+        songMetas = SongMetaManager.Instance.SongMetas;
+        List<PlayerProfile> playerProfiles = SettingsManager.Instance.Settings.PlayerProfiles;
+
+        songRouletteController = FindObjectOfType<SongRouletteController>();
+        songRouletteController.SongSelectSceneController = this;
+
+        statsManager = StatsManager.Instance.Statistics;
+
+        InitSongRoulette();
+
+        // Show a message when no songs have been found.
+        noSongsFoundMessage.SetActive(songMetas.IsNullOrEmpty());
+    }
+
+    public void OnHotSwapFinished()
+    {
+        InitSongRoulette();
+    }
+
+    private void InitSongRoulette()
+    {
+        songRouletteController.SetSongs(songMetas);
+        if (sceneData.SongMeta != null)
+        {
+            songRouletteController.SelectSong(sceneData.SongMeta);
+        }
+
+        songRouletteController.Selection.Subscribe(newValue => OnNewSongSelection(newValue));
+    }
+
+    private void OnNewSongSelection(SongSelection selection)
+    {
+        SongMeta selectedSong = selection.SongMeta;
+        if (selectedSong == null)
+        {
+            SetEmptySongDetails();
+            return;
+        }
+
+        artistText.SetText(selectedSong.Artist);
+        songTitleText.text = selectedSong.Title;
+        songCountText.text = (selection.SongIndex + 1) + "/" + selection.SongsCount;
+
+        //Display local highscore
+        highscoreLocalPlayer.SetText("");
+        highscoreLocalScore.SetText("0");
+        LocalStatistic localStats = statsManager.GetLocalStats(selectedSong);
+        SongStatistic localTopScore; 
+        if (localStats != null)
+        {
+            localTopScore = localStats.StatsEntries.TopScore;
+
+            if (localTopScore != null)
+            {
+                Debug.Log("Found local highscore: " + localTopScore.PlayerName + " " + localTopScore.Score.ToString());
+                highscoreLocalPlayer.SetText(localTopScore.PlayerName);
+                highscoreLocalScore.SetText(localTopScore.Score.ToString());
+            }
+        }
+
+        //Display web highscore
+        highscoreWebPlayer.SetText("");
+        highscoreWebScore.SetText("0");
+        WebStatistic webStats = statsManager.GetWebStats(selectedSong);
+        SongStatistic webTopScore;
+        if (webStats != null)
+        {
+            webTopScore = webStats.StatsEntries.TopScore;
+
+            if (webTopScore != null)
+            {
+                Debug.Log("Found web highscore: " + webTopScore.PlayerName + " " + webTopScore.Score.ToString());
+                highscoreWebPlayer.SetText(webTopScore.PlayerName);
+                highscoreWebScore.SetText(webTopScore.Score.ToString());
+            }
+        }
+
+        bool hasVideo = !string.IsNullOrEmpty(selectedSong.Video);
+        videoIndicator.SetActive(hasVideo);
+
+        bool isDuet = selectedSong.VoiceNames.Count > 1;
+        duetIndicator.SetActive(isDuet);
+    }
+
+    private void StartSingScene(SongMeta songMeta)
+    {
+        SingSceneData singSceneData = new SingSceneData();
+        singSceneData.SelectedSongMeta = songMeta;
+
+        List<PlayerProfile> selectedPlayerProfiles = playerProfileListController.GetSelectedPlayerProfiles();
+        if (selectedPlayerProfiles.IsNullOrEmpty())
+        {
+            UiManager.Instance.CreateWarningDialog("No player selected", "Select a player profile for singing.\n New player profiles can be create in the settings.");
+            return;
+        }
+        singSceneData.SelectedPlayerProfiles = selectedPlayerProfiles;
+
+        PlayerProfileToMicProfileMap playerProfileToMicProfileMap = playerProfileListController.GetSelectedPlayerProfileToMicProfileMap();
+        singSceneData.PlayerProfileToMicProfileMap = playerProfileToMicProfileMap;
+
+        SceneNavigator.Instance.LoadScene(EScene.SingScene, singSceneData);
+    }
+
+    private SongSelectSceneData CreateDefaultSceneData()
+    {
+        SongSelectSceneData sceneData = new SongSelectSceneData();
+        return sceneData;
+    }
+
+    private void SetEmptySongDetails()
+    {
+        artistText.SetText("");
+        songTitleText.text = "";
+        songCountText.text = "0/0";
+        videoIndicator.SetActive(false);
+        duetIndicator.SetActive(false);
+    }
+
+    public void OnNextSong()
+    {
+        songRouletteController.SelectNextSong();
+    }
+
+    public void OnPreviousSong()
+    {
+        songRouletteController.SelectPreviousSong();
+    }
+
+    public void StartSingScene()
+    {
+        if (SelectedSong != null)
+        {
+            StartSingScene(SelectedSong);
+        }
+    }
+
+    public void OnSearchTextChanged()
+    {
+        SongMeta lastSelectedSong = SelectedSong;
+        string searchText = searchTextInputField.Text.ToLower();
+        if (string.IsNullOrEmpty(searchText))
+        {
+            songRouletteController.SetSongs(songMetas);
+            if (lastSelectedSong != null)
+            {
+                songRouletteController.SelectSong(lastSelectedSong);
+            }
+            else if (selectedSongBeforeSearch != null)
+            {
+                songRouletteController.SelectSong(selectedSongBeforeSearch);
+            }
+        }
+        else
+        {
+            switch (searchTextInputField.SearchMode)
+            {
+                case SearchInputField.ESearchMode.BySongTitle:
+                    DoSearch((songMeta) => songMeta.Title.ToLower().Contains(searchText));
+                    break;
+                case SearchInputField.ESearchMode.ByArtist:
+                    DoSearch((songMeta) => songMeta.Artist.ToLower().Contains(searchText));
+                    break;
+            }
+        }
+    }
+
+    private void DoSearch(Func<SongMeta, bool> condition)
+    {
+        List<SongMeta> matchingSongs = songMetas.Where(condition).ToList();
+        songRouletteController.SetSongs(matchingSongs);
+    }
+
+    public void EnableSearch(SearchInputField.ESearchMode searchMode)
+    {
+        selectedSongBeforeSearch = SelectedSong;
+
+        searchTextInputField.Show();
+        searchTextInputField.RequestFocus();
+        searchTextInputField.SearchMode = searchMode;
+        searchTextInputField.Text = "";
+    }
+
+    public void DisableSearch()
+    {
+        searchTextInputField.Text = "";
+        searchTextInputField.Hide();
+    }
+
+    public bool IsSearchEnabled()
+    {
+        return searchTextInputField.isActiveAndEnabled;
+    }
+}
+>>>>>>> parent of 5e3c645... Improved highscore display in the song select
